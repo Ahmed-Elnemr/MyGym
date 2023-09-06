798127e2c12dde03f6396208044243a0c02d221e
nemr <ahmedelnemr064@gmail.com>
2023-09-07 00:22:47 +0300

    create migration and model and relation in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    #######################    relation ##################33
+    public function scheduledClasses()
+    {
+        return $this->hasMany(ScheduledClass::class, 'instructor_id');
+    }
 }
