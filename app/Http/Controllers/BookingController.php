d2fdbed6e1a417758e2e75a531696fe606d5fb42
nemr <ahmedelnemr064@gmail.com>
2023-09-10 06:18:39 +0300

    make view member.book $ install telescope & impelemnt eager loading by use with when index all classes

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\ScheduledClass;
 
 class BookingController extends Controller
 {
-    //
+    public function create()
+    {
+        $scheduledClasses = ScheduledClass::where('date_time', '>', now())
+            ->with('classType', 'instructor')
+            ->oldest()->get();
+        return view('member.book')->with('scheduledClasses', $scheduledClasses);
+    }
 }
