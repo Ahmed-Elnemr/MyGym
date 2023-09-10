7b3ce1215e8f4140973f2e4b947aac539232edac
nemr <ahmedelnemr064@gmail.com>
2023-09-10 04:24:56 +0300

    add seeder and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\ScheduledClass;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\ScheduledClassSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class,
+            ClassTypeSeeder::class,
+            ScheduledClassSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'ahmed',
+            'email' => 'instructor@instructor.com',
+            'role' => 'instructor',
+            'password' => Hash::make('instructor')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'role' => 'admin',
+            'password' => Hash::make('Admin')
+
+        ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'member@member.com',
+            'role' => 'member',
+            'password' => Hash::make('member')
+
+        ]);
+
+        User::factory()->count(10)->create();
+        User::factory()->count(10)->create([
+            'role' => 'instructor',
+        ]);
+    }
+}
